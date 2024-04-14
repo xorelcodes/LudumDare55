535ebc83bb35383342289c9c0cff7fa10d4ff365
xorelcodes <drbridgewate@gmail.com>
2024-04-13 22:35:09 -0400

    Added placeholder player sprite, simple player controller added.

--- Scripts/Controllers/player_controller.gd
+++ Scripts/Controllers/player_controller.gd
@@ -1,11 +1,29 @@
-extends Node
+extends CharacterBody2D
 
+const GRAVITY = 0.0
+const WALK_SPEED = 200
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _physics_process(delta):
+	velocity.y += delta * GRAVITY
+
+	if Input.is_action_pressed("ui_up"):
+		velocity.y = -WALK_SPEED
+	elif Input.is_action_pressed("ui_down"):
+		velocity.y = WALK_SPEED
+	else:
+		velocity.y = 0
+
+	if Input.is_action_pressed("ui_left"):
+		velocity.x = -WALK_SPEED
+	elif Input.is_action_pressed("ui_right"):
+		velocity.x =  WALK_SPEED
+	else:
+		velocity.x = 0
+
+	# "move_and_slide" already takes delta time into account.
+	move_and_slide()
